3601885aedf718c1c1e45f1c8d7510727df1c7a1
Nazim Lachter <lachtater@gmail.com>
2017-11-16 11:32:42 +0100

    Adding commander with an example

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+
+program
+    .option('--no-sauce', 'Remove sauce')
+    .parse(process.argv);
+
+console.log('you ordered a pizza');
+if(program.sauce) console.log('  with sauce');
+else console.log(' without sauce');
